610c6cce84f5dda1f0e23ce0a212da8b3ab5968f
Stephanie Phillips <steph@ladymacbeth.uk>
2018-10-30 18:04:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 /* Enter the code to remove the main node element under this comment */
+let x = document.querySelector('main') 
+x.remove()
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+let newHeader = null;
+newHeader = document.createElement('header')
+document.body.append(newHeader)
+
+var element = document.createElement('h1')
+element.id = 'victory'
+element.innerHTML = 'Steph, is the champion!'
+newHeader.append(element);
